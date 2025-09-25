143c620b38b5e1467627560e00733569d7d7e4e4
Rajeshwari-Mamidipally <147687110+Rajeshwari-Mamidipally@users.noreply.github.com>
2025-09-25 07:54:30 +0530

    Update main.cpp

--- Rajeshwari_Sep24/Rajeshwari_Sep24_task2/main.cpp
+++ Rajeshwari_Sep24/Rajeshwari_Sep24_task2/main.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include "Date.h"
 int main(){
-    Date date1(7,4,2004); //creating object
+    std::cout<<"Enter date as month,day,year : ";
+    int month,day,year;
+    std::cin>>month>>day>>year;
+    Date date1(month,day,year); //creating object
     std::cout<<"Date1 = ";
     date1.print();
     date1.nextDay();
     std::cout<<"\n";
     date1.print();
     return 0;
+
 }
