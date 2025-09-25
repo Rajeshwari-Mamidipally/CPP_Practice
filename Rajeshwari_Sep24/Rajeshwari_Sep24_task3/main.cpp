adedd17793a3f98fe510292b19bec11ad2ccf20b
Rajeshwari-Mamidipally <147687110+Rajeshwari-Mamidipally@users.noreply.github.com>
2025-09-25 07:59:00 +0530

    Update main.cpp

--- Rajeshwari_Sep24/Rajeshwari_Sep24_task3/main.cpp
+++ Rajeshwari_Sep24/Rajeshwari_Sep24_task3/main.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include "DateAndTime.h"
 int main(){
-    DateAndTime dt(4,30,2004,23,59,59); //creating object t
+    std::cout<<"Enter date (as month,day,year) and time(as  hour,minute,second) : ";
+    int month,day,year,hour,min,sec;
+    std::cin>>month>>day>>year>>hour>>min>>sec;
+    DateAndTime dt(month,day,year,hour,min,sec); //creating object t
     std::cout << "Starting date and time: \n";
     dt.printDateAndTime();
     dt.tick();
     std::cout<<"\nAfter tick function date and time \n";
     dt.printDateAndTime();
     return 0;
-
-   
 }
