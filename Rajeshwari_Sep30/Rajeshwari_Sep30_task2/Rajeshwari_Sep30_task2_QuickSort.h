37cf794f7ce0e4eec662b8443da2e8e611fd7aaf
Rajeshwari-Mamidipally <147687110+Rajeshwari-Mamidipally@users.noreply.github.com>
2025-10-03 10:59:50 +0530

    Update Rajeshwari_Sep30_task2_QuickSort.h

--- Rajeshwari_Sep30/Rajeshwari_Sep30_task2/Rajeshwari_Sep30_task2_QuickSort.h
+++ Rajeshwari_Sep30/Rajeshwari_Sep30_task2/Rajeshwari_Sep30_task2_QuickSort.h
@@ -1,6 +1,7 @@
-#ifndef QUICKSORT_H
-#define QUICKSORT_H
+#ifndef RAJESHWARI_SEP30_TASK2_QUICKSORT_H
+#define RAJESHWARI_SEP30_TASK2_QUICKSORT_H
 void swap(int &a,int &b);
 void quickSort(int[] ,int,int);
 int partitioning(int[],int,int);
+
 #endif
