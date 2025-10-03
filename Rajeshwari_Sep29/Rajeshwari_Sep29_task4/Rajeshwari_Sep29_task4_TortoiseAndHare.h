cc081be98e397ede54a9ab7ae851847b13276dde
Rajeshwari-Mamidipally <147687110+Rajeshwari-Mamidipally@users.noreply.github.com>
2025-10-03 10:55:42 +0530

    Update Rajeshwari_Sep29_task4_TortoiseAndHare.h

--- Rajeshwari_Sep29/Rajeshwari_Sep29_task4/Rajeshwari_Sep29_task4_TortoiseAndHare.h
+++ Rajeshwari_Sep29/Rajeshwari_Sep29_task4/Rajeshwari_Sep29_task4_TortoiseAndHare.h
@@ -1,6 +1,7 @@
-#ifndef TORTOISEANDHARE_H
-#define TORTOISEANDHARE_H
+#ifndef RAJESHWARI_SEP29_TASK4_TORTOISEANDHARE_H
+#define RAJESHWARI_SEP29_TASK4_TORTOISEANDHARE_H
 void moveTortoise(int* pos);
 void moveHare(int* pos);
+
 
 #endif
